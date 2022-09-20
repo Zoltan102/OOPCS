24fd53ea626ec116c0102d8fe48bc985542cb048
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-09-20 17:14:56 +0200

    Point.cs -constuctors

---
+++ Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP
+{
+    public class Point
+    {
+        private int x;
+        private int y;
+
+        public Point()
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+
+        public Point(int num)
+        {
+            Random rnd = new Random();
+            this.x = rnd.Next(-num, num + 1);
+            this.y = rnd.Next(-num, num + 1);
+        }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
